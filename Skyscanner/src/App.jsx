7e1c79e3ec7b56dee73819c9e0ab0c9bd8b6cb56
Ali Murtaza <syedalim9087@gmail.com>
2023-01-09 22:45:36 +0100

    Created React App

---
+++ Skyscanner/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Welcome to the page Or Ali and Egnoel</div>
+  )
+}
+
+export default App
