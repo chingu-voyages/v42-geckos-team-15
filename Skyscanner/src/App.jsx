526b21430897ffa4cb260824de8c2702ffd3ba52
ordegani <oradeganii@gmail.com>
2023-01-18 19:40:14 +0200

    adding Main component to App

--- Skyscanner/src/App.jsx
+++ Skyscanner/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Main from './components/Main/Main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>Welcome to the page Or Ali and Egnoel</div>
+    <div className="pageWrap">
+      Welcome to the page Or Ali and Egnoel
+    <Main/>
+    </div>
   )
 }
 
