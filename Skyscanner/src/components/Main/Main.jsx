9047e6e8a19a58a2ebb114d5e6e62762656c82c1
ordegani <oradeganii@gmail.com>
2023-01-18 21:37:29 +0200

    chaning departure  params

--- Skyscanner/src/components/Main/Main.jsx
+++ Skyscanner/src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useState } from "react";
 
-function Main () {
+function Main() {
     // str type
     const [Departure, setDeparture] = useState('');
     const [Destination, setDestination] = useState('');
@@ -11,15 +11,15 @@ function Main () {
     // DepartDate, Returndate can't be in the past
     // DepartDate is 5 days from today - use built in function new Date()
     // obj type, int type
-    const [DepartDateYear , setDepartDateYear ] = useState(today.getFullYear());
-    const [DepartDateMonth , setDepartDateMonth ] = useState(today.getMonth());
-    const [DepartDateDay , setDepartDateDay ] = useState(today.getDate());
+    const [DepartDateYear, setDepartDateYear] = useState(today.getFullYear());
+    const [DepartDateMonth, setDepartDateMonth] = useState(today.getMonth());
+    const [DepartDateDay, setDepartDateDay] = useState(today.getDate());
     // Returndate can't be earlier than DepartDate
-    const [ReturndateYear , setReturndateYear ] = useState(DepartDateYear);
-    const [ReturndateMonth , setReturndateMonth ] = useState(DepartDateMonth);
-    const [ReturndateDay , setReturndateDay ] = useState(DepartDateDay);
+    const [ReturndateYear, setReturndateYear] = useState(DepartDateYear);
+    const [ReturndateMonth, setReturndateMonth] = useState(DepartDateMonth);
+    const [ReturndateDay, setReturndateDay] = useState(DepartDateDay);
     // int type
-    const [numberOfTravelers , setNumberOfTravelers ] = useState(1);
+    const [numberOfTravelers, setNumberOfTravelers] = useState(1);
 
 }
 
