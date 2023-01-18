e8d3dc9df5a19e6f053b3d918374969119ea7833
ordegani <oradeganii@gmail.com>
2023-01-18 21:36:56 +0200

    chaning return params

--- Skyscanner/src/components/Main/Main.jsx
+++ Skyscanner/src/components/Main/Main.jsx
@@ -5,12 +5,15 @@ function Main () {
     // str type
     const [Departure, setDeparture] = useState('');
     const [Destination, setDestination] = useState('');
+
+    const today = new Date();
+    console.log(today);
     // DepartDate, Returndate can't be in the past
     // DepartDate is 5 days from today - use built in function new Date()
     // obj type, int type
-    const [DepartDateYear , setDepartDateYear ] = useState(0);
-    const [DepartDateMonth , setDepartDateMonth ] = useState(0);
-    const [DepartDateDay , setDepartDateDay ] = useState(0);
+    const [DepartDateYear , setDepartDateYear ] = useState(today.getFullYear());
+    const [DepartDateMonth , setDepartDateMonth ] = useState(today.getMonth());
+    const [DepartDateDay , setDepartDateDay ] = useState(today.getDate());
     // Returndate can't be earlier than DepartDate
     const [ReturndateYear , setReturndateYear ] = useState(DepartDateYear);
     const [ReturndateMonth , setReturndateMonth ] = useState(DepartDateMonth);
@@ -18,8 +21,6 @@ function Main () {
     // int type
     const [numberOfTravelers , setNumberOfTravelers ] = useState(1);
 
-    const today = new Date();
-    console.log(today);
 }
 
 export default Main;
